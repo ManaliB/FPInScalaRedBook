79d834652c719e18d9b4cfeaff294c7c146adbd3
Manali Bhosale <manali.bhosale@qomplx.com>
2020-01-19 11:08:24 -0500

    Chapter 2 lessons: Used anonymous function.

--- src/test/scala/chapter2/TwoSpec.scala
+++ src/test/scala/chapter2/TwoSpec.scala
@@ -3,41 +3,34 @@ package chapter2
 import org.scalatest.{FlatSpec, Matchers}
 
 class TwoSpec extends FlatSpec with Matchers {
-  private def >(a: Int, b: Int): Boolean = a > b
-  private def <(a: Int, b: Int): Boolean = a < b
-
-  private def ==(a: Int, b: Int): Boolean = a == b
-  private def eq(a: String, b: String): Boolean = a == b
-
-
   "isSorted" should "return true for only one element" in {
-    Two.isSorted(Array(1), >) shouldEqual true
+    Two.isSorted(Array(1), (a: Int, b: Int) => a > b) shouldEqual true
   }
   "isSorted" should "return false for descending" in {
-    Two.isSorted(Array(1,2,3,4,5,6), >) shouldEqual false
+    Two.isSorted(Array(1,2,3,4,5,6), (a: Int, b: Int) => a > b) shouldEqual false
   }
   "isSorted" should "return false for descending 2" in {
-    Two.isSorted(Array(1,2,3,2,1), >) shouldEqual false
+    Two.isSorted(Array(1,2,3,2,1), (a: Int, b: Int) => a > b) shouldEqual false
   }
   "isSorted" should "return true for descending" in {
-    Two.isSorted(Array(6,5,4,3,2,1), >) shouldEqual true
+    Two.isSorted(Array(6,5,4,3,2,1), (a: Int, b: Int) => a > b) shouldEqual true
   }
   "isSorted" should "return true for ascending" in {
-    Two.isSorted(Array(1,2,3,4,5,6), <) shouldEqual true
+    Two.isSorted(Array(1,2,3,4,5,6), (a: Int, b: Int) => a < b) shouldEqual true
   }
   "isSorted" should "return false for ascending" in {
-    Two.isSorted(Array(6,5,4,3,2,1), <) shouldEqual false
+    Two.isSorted(Array(6,5,4,3,2,1), (a: Int, b: Int) => a < b) shouldEqual false
   }
   "isSorted" should "return false for ==" in {
-    Two.isSorted(Array(1,1,2,3,1), ==) shouldEqual false
+    Two.isSorted(Array(1,1,2,3,1), (a: Int, b: Int) => a == b) shouldEqual false
   }
   "isSorted" should "return true for ==" in {
-    Two.isSorted(Array(1,1,1), ==) shouldEqual true
+    Two.isSorted(Array(1,1,1), (a: Int, b: Int) => a == b) shouldEqual true
   }
   "isSorted" should "return true for String eq" in {
-    Two.isSorted(Array("Abc", "Abc", "Abc", "Abc"), eq) shouldEqual true
+    Two.isSorted(Array("Abc", "Abc", "Abc", "Abc"), (a: String, b: String) => a == b) shouldEqual true
   }
   "isSorted" should "return true for empty Strings eq" in {
-    Two.isSorted(Array("", ""), eq) shouldEqual true
+    Two.isSorted(Array("", ""), (a: String, b: String) => a == b) shouldEqual true
   }
 }
