a51027b4b7b2d88f29d4ac7e42126276f8dc2a68
Manali Bhosale <manali.bhosale@qomplx.com>
2021-01-02 16:02:19 -0500

    Added comments

--- src/test/scala/chapter3/TreeSpec.scala
+++ src/test/scala/chapter3/TreeSpec.scala
@@ -4,19 +4,21 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class TreeSpec extends FlatSpec with Matchers {
 
+// 3.25
   "size" should "find the size of the tree" in {
     Tree.size(Leaf(2)) shouldBe 1
     Tree.size(Branch(Leaf(4), Leaf(2))) shouldBe 3
     Tree.size(Branch(Branch(Leaf(5), Leaf(17)), Leaf(-66))) shouldBe 5
   }
 
+  // 3.26
   "maximum" should "find maximum integer in an integer tree" in {
     Tree.maximum(Leaf(2)) shouldBe 2
     Tree.maximum(Branch(Leaf(4), Leaf(2))) shouldBe 4
     Tree.maximum(Branch(Branch(Leaf(5), Leaf(17)), Leaf(-66))) shouldBe 17
   }
 
-
+  // 3.27
   "depth" should "find the depth of a tree" in {
     Tree.depth(Leaf(2)) shouldBe 1
     Tree.depth(Branch(Leaf(4), Leaf(2))) shouldBe 2
@@ -25,8 +27,7 @@ class TreeSpec extends FlatSpec with Matchers {
     Tree.depth(Branch(Branch(Branch(Leaf(55), Leaf(90)), Leaf(17)), Leaf(-66))) shouldBe 4
   }
 
-
-
+  // 3.28
   "map" should "multiply all elements in tree by 2" in {
     Tree.map(Leaf(2))(_ * 2) shouldBe Leaf(4)
     Tree.map(Branch(Leaf(4), Leaf(2)))(_ * 2) shouldBe Branch(Leaf(8), Leaf(4))
@@ -34,5 +35,4 @@ class TreeSpec extends FlatSpec with Matchers {
     Tree.map(Branch(Branch(Leaf(5), Leaf(17)), Leaf(-66)))(_ * 2) shouldBe Branch(Branch(Leaf(10), Leaf(34)), Leaf(-132))
     Tree.map(Branch(Branch(Branch(Leaf(55), Leaf(0)), Leaf(17)), Leaf(-66)))(_ * 2) shouldBe Branch(Branch(Branch(Leaf(110), Leaf(0)), Leaf(34)), Leaf(-132))
   }
-
 }
