038122333b88d83f80d279fac20170c31b3cdbb8
Manali Bhosale <manali.bhosale@qomplx.com>
2021-01-02 15:38:45 -0500

    3.25: size of a tree

--- src/test/scala/chapter3/TreeSpec.scala
+++ src/test/scala/chapter3/TreeSpec.scala
@@ -4,9 +4,16 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class TreeSpec extends FlatSpec with Matchers {
 
+  "size" should "find the size of the tree" in {
+    Tree.size(Leaf(2)) shouldBe 1
+    Tree.size(Branch(Leaf(4), Leaf(2))) shouldBe 3
+    Tree.size(Branch(Branch(Leaf(5), Leaf(17)), Leaf(-66))) shouldBe 5
+  }
+
   "maximum" should "find maximum integer in an integer tree" in {
     Tree.maximum(Leaf(2)) shouldBe 2
     Tree.maximum(Branch(Leaf(4), Leaf(2))) shouldBe 4
     Tree.maximum(Branch(Branch(Leaf(5), Leaf(17)), Leaf(-66))) shouldBe 17
   }
+
 }
