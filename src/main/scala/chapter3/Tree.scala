d747108526699591808cc52aa6141d76b03f03cf
Manali Bhosale <manali.bhosale@qomplx.com>
2021-01-02 15:39:36 -0500

    3.25 and 3.26: size of and max in a tree

---
+++ src/main/scala/chapter3/Tree.scala
@@ -0,0 +1,19 @@
+package chapter3
+
+import scala.math.Ordering.Implicits.infixOrderingOps
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+  def size[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 1
+    case Branch(left, right) => 1 + size(left) + size(right)
+  }
+
+  def maximum(tree: Tree[Int]): Int = tree match {
+    case Leaf(x) => x
+    case Branch(left, right) => maximum(left) max maximum(right)
+  }
+}
